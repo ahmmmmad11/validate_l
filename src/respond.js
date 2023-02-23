1c8ff420a8d9be706992f0da207f8ff7ec2bab4b
Ahmed Mohammed <ahmed.moh@flixtechnology.com>
2023-02-23 14:53:07 +0200

    replace :value blace holder with actual value

--- src/respond.js
+++ src/respond.js
@@ -1,37 +1,47 @@
 const path = require('path')
 const messages = require("./messages");
 
-//determine if a field has an alliance
+//determine if a field has an alias
 //if so return the alliance
 const field_name = (item, user_fields) => {
     if (item in user_fields){
         return user_fields[item]
     }
     return item
+};
+
+const value = (message, val) => {
+    return message.replace(':value', val);
 }
 
 module.exports = (conf, rule, item, extra = {}) => {
     let localization = null;
 
     try {
-        localization = require(path.resolve(`validations/${conf.lang}.js`))
+        localization = require(path.resolve(`lang/validations/${conf.lang}.js`));
     }
     catch (e) {
         localization = null
     }
 
+    let message = '';
+
     //take validation messages from the built-in messages.js file inside the package
-    conf.response[item] = messages[rule].replace(':item', field_name(item, conf.fields_alias));
+    message = messages[rule].replace(':item', field_name(item, conf.fields_alias));
+    message = value(message, extra);
 
     //take validation messages from the localization files
     if (localization != null && typeof(localization) == 'object') {
-        console.log(localization.messages)
-        conf.response[item] = localization.messages[rule].replace(':item', field_name(item, localization.fields));
+        message = localization.messages[rule].replace(':item', field_name(item, localization.fields));
+        message = value(message, extra);
     }
     
     //check if a developer added another message for the validation
     //if so change the default message with altered one
     if (`${item}.${rule}` in conf.message_alias) {
-        conf.response[item] = conf.message_alias[`${item}.${rule}`].replace(':item', field_name(item, conf.fields_alias));
+        message = conf.message_alias[`${item}.${rule}`].replace(':item', field_name(item, conf.fields_alias));
+        message = value(message, extra);
     }
+
+    conf.response[item] = message;
 }
