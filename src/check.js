cccc244aac02b9473d21266f1246c3df9eaf2796
Ahmed Mohammed <ahmed.moh@flixtechnology.com>
2023-02-23 11:14:06 +0200

    edited check function to handel response writing

--- src/check.js
+++ src/check.js
@@ -1,11 +1,58 @@
-const validations = require('./validations')
+const validations = require('./validations');
+const respond = require('./respond');
 
-module.exports = (req, res, items, conf) => {
-    let role = term.split(':')
-    if (role[0] in validations) {
-        if (role.length > 1) {
-            return validations[term](conf, req, item, role[1], items[item])
+const getFunctionParameters = (fn) => {
+    return /\((.*?)\)/.exec(validations[fn].toString())[1];
+};
+
+const getArgs = (argsList, field, req, appendix) => {
+    let args = [];
+
+    for (let arg of argsList) {
+        if (arg === 'item') {
+            args.push(req.body[field]);
+            continue;
         }
-        return validations[term](conf, req, item)
+
+        if (arg === 'field') {
+            args.push(field);
+            continue;
+        }
+
+        if (arg === 'body') {
+            args.push(req.body);
+            continue;
+        }
+
+        if (arg === 'req') {
+            args.push(req);
+            continue;
+        }
+
+        args.push(appendix);
+    }
+
+    return args;
+};
+
+module.exports = (req, field, rule, conf) => {
+    rule = rule.split(':')
+
+    if (rule[0] in validations) {
+
+        if(
+            validations[rule[0]](...getArgs(
+                getFunctionParameters(rule[0]).split(','),
+                field,
+                req,
+                rule[1]
+            ))
+        ) {
+            return true;
+        }
+
+        respond(conf, rule[0], field, rule[1]);
+
+        return false;
     }
 }
